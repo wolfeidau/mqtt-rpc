ad09dc573f1d63aa97dc7c3c03bd55b84942e045
Mark Wolfe <mark@wolfe.id.au>
2013-09-06 04:30:59 +1000

    Fixed issue with cb being invoked twice and more tests.

--- test/combined.js
+++ test/combined.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var crypto = require('crypto');
 var chai = require('chai');
 var mqtt = require('mqtt');
 var debug = require('debug')('test:mqtt-rpc:combined');
@@ -8,32 +7,35 @@ var expect = chai.expect;
 
 var mqttrpc = require('../index.js');
 
-var client = mqttrpc.client(mqtt.createClient());
-var server = mqttrpc.server(mqtt.createClient());
-
-var mqttclient;
-
-
 describe('combined', function () {
 
   it('server respond to a request from the client', function (done) {
 
     var prefix = "$RPC/time3";
 
-    server.provide(prefix, 'localtime', function(args, cb){
+    var server = mqttrpc.server(mqtt.createClient());
+
+    server.provide(prefix, 'localtime', function (args, cb) {
       debug('localtime');
       cb(null, new Date());
     });
 
-    client.callRemote(prefix, 'localtime', {}, function(err, data){
+    var client = mqttrpc.client(mqtt.createClient());
 
+    client.callRemote(prefix, 'localtime', {}, function (err, data) {
       debug('callRemote', err, data);
       expect(data).to.exist;
       expect(err).to.not.exist;
-      done();
 
+      client.callRemote(prefix, 'localtime', {}, function (err, data) {
+        debug('callRemote', err, data);
+        expect(data).to.exist;
+        expect(err).to.not.exist;
+        done();
+      });
 
     });
+
 
   });
 
